26483b81c2b9f98ee5019be5d23f04cb6b38b053
Nico Blasi <nicolas.blasi.78@gmail.com>
2022-06-30 16:36:52 -0300

    commit para subir desafio

---
+++ main.js
@@ -0,0 +1,49 @@
+const nombre = prompt('Ingresa tu nombre');
+
+alert(`Hola ${nombre}, bienvenido a la peluqueria Liverpool`);
+
+let corte= prompt('¿Qué tipo de corte queres? (Para corte masculino: 1, para corte femenino: 2)');
+
+let precio = 0;
+
+do{
+
+    if(corte == 1){
+
+        precio = 600;
+
+        alert(`El precio total del corte es de ${precio}`);
+
+    } else if(corte == 2){
+
+        precio = 800;
+
+        alert(`El precio total del corte es de ${precio}`);
+
+    } else {
+
+        alert('El corte no existe');
+
+        corte = prompt('¿Qué tipo de corte desea? (Para corte masculino: 1, para corte femenino: 2)');
+
+    }
+
+} while(corte != 1 && corte != 2);
+
+let pago = prompt('Con cuanto vas a pagar ?');
+
+do {
+
+    if(pago < precio){
+
+        alert('El pago es insuficiente');
+
+        pago = parseInt(prompt('Con cuanto vas a pagar ?'));
+
+    } else {
+
+        alert(`Tu pago es de ${pago}, el cambio es de ${pago - precio}, gracias por cortarte el pelo con Liverpool`);
+
+    }
+
+} while (pago < precio);
